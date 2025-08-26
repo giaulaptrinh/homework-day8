9b0cf368ce80e127eeb08c3d8bbe02af84d4f920
Giau Lee <92901576+giaulaptrinh@users.noreply.github.com>
2025-08-26 09:36:10 +0700

    Remove vueDevTools plugin to fix startup error

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueRouter from "unplugin-vue-router/vite";
-import vueDevTools from "vite-plugin-vue-devtools";
 import layouts from "vite-plugin-vue-layouts";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
     vueRouter({
       extensions: [".vue", ".md"],
     }),
